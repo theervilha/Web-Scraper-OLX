98edda2895b322cacfd0fe0e898ba275c2b361da
Ueniry Felipe <ueniry.f@gmail.com>
2022-09-17 21:42:01 -0300

    Saving extracted products to json; now you can put the url on url.txt file.

--- main.py
+++ main.py
@@ -2,22 +2,31 @@ from Scrapers.SeleniumOLX import SeleniumOLX
 from Scrapers.ScrapyOLX import get_products
 
 from datetime import datetime
+import json
 
-url = 'https://rn.olx.com.br/moveis?pe=300&ps=100&q=mesa'
+with open('url.txt', 'r') as f:
+    url = f.read()
 
-# Web Scraping with Selenium
-SeleniumOLX = SeleniumOLX()
-#products = SeleniumOLX.search_in_url(url=url)
-
-## If you wanna get products until '09/17 18:00', just do:
-last_date_example = datetime(1900, 9, 17, 18, 0) # 1900 because we are desconsidering the year. The website don't give us the year of products
-products = SeleniumOLX.search_in_url(url=url, get_until_this_date=last_date_example)
-
-
-
-# # Web Scraping with Scrapy
-# products = get_products(url=url)
+# # Web Scraping with Selenium
+# SeleniumOLX = SeleniumOLX()
+# products = SeleniumOLX.search_in_url(url=url)
 
 # ## If you wanna get products until '09/17 18:00', just do:
 # last_date_example = datetime(1900, 9, 17, 18, 0) # 1900 because we are desconsidering the year. The website don't give us the year of products
-# products = get_products(url=url, get_until_this_date=last_date_example)
+# products = SeleniumOLX.search_in_url(url=url, get_until_this_date=last_date_example)
+
+
+
+# Web Scraping with Scrapy
+products = get_products(url=url)
+
+# If you wanna get products until '09/17 18:00', just do:
+#last_date_example = datetime(1900, 9, 17, 18, 0) # 1900 because we are desconsidering the year. The website don't give us the year of products
+#products = get_products(url=url, get_until_this_date=last_date_example)
+
+
+def save_to_json(products):
+    with open('extracted_products.json', 'w', encoding='utf-8') as f:
+        json.dump(products, f, ensure_ascii=False, indent=4, default=str)
+
+save_to_json(products)
